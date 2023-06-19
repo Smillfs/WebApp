2b23ae35520af908599c126dee64664abcbcdbd7
Smillfs <137096341+Smillfs@users.noreply.github.com>
2023-06-20 01:17:35 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Text.RegularExpressions;
+
+
+namespace WebApp
+{
+    public partial class MainWindow : Window
+    {
+        private static string urlPattern = "^(http|https)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonGo_Click(object sender, RoutedEventArgs e)
+        {
+            if (Regex.IsMatch(addressBar.Text, urlPattern))
+            {
+                webView.CoreWebView2.Navigate(addressBar.Text);
+            } else {
+                MessageBox.Show("Вы ввели некроектный url, попробуйте ещё раз!");
+            }
+        }
+    }
+}
